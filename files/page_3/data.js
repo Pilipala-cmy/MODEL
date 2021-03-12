1583f3a1afc7380abbed81acc3c4d7c92c57862f
ChenMy <1072876025@qq.com>
2021-03-12 13:43:01 +0800

    finish

---
+++ files/page_3/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p],q,_(r,s,t,u,v,w,x,_(),y,_(z,A,B,C,D,_(E,F,G,H),I,null,J,C,K,C,L,M,N,null,O,P,Q,R,S,T,U,P),V,_(),W,_(),X,_(Y,[])),Z,_(),ba,_());}; 
+var b="url",c="page_3.html",d="generationDate",e=new Date(1615527332597.55),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="title",o="author",p="abstract",q="page",r="packageId",s="efe7b757c74e49cd9bb968a96b85e341",t="type",u="Axure:Page",v="name",w="Page 3",x="notes",y="style",z="baseStyle",A="627587b6038d43cca051c114ac41ad32",B="pageAlignment",C="near",D="fill",E="fillType",F="solid",G="color",H=0xFFFFFFFF,I="image",J="imageHorizontalAlignment",K="imageVerticalAlignment",L="imageRepeat",M="auto",N="favicon",O="sketchFactor",P="0",Q="colorStyle",R="appliedColor",S="fontName",T="Applied Font",U="borderWidth",V="adaptiveStyles",W="interactionMap",X="diagram",Y="objects",Z="masters",ba="objectPaths";
+return _creator();
+})());
